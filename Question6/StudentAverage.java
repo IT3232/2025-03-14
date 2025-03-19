f63eeb28026c0804ff6a2368f6986fe3864edcb9
Dinuki Prarthana <139613301+Dinuki85@users.noreply.github.com>
2025-03-19 15:01:16 +0000

    Craete Student Class

---
+++ Question6/StudentAverage.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class Student {
+    String name;
+    int marks;
+
+    
+    public Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
